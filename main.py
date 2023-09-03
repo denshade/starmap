9215c1420abbc8be3fb1f17479cacfbff5f27747
denshade <lieven.vaneeckhaute@gmail.com>
2023-09-03 21:01:56 +0200

    first stab

---
+++ main.py
@@ -0,0 +1,106 @@
+import csv
+import math
+import re
+import os
+
+
+def write_coordinates_to_csv(filename, coordinates):
+    with open(filename, 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['x', 'y', 'z'])  # Write the header row
+
+        for x, y, z in coordinates:
+            writer.writerow([x, y, z])
+
+def pad(s):
+    c = s.lstrip('0')
+    if len(c) == 0 or c == "00.0":
+        return 0
+    return c
+def convert_right_inclination_to_degrees(right_inclination):
+    components = re.findall(r'[0-9]+(?:\.[0-9]+)?', right_inclination)
+    try:
+        hours = int(pad(components[0]))  # Extract the numeric part of hours and convert it to an integer
+        minutes = int(pad(components[1]))  # Extract the numeric part of minutes and convert it to an integer
+        seconds = float(pad(components[2]))  # Extract the numeric part of seconds and convert it to a float
+    except:
+        raise Exception(right_inclination)
+    # Convert each component to degrees
+    hours_deg = hours * 15
+    minutes_deg = minutes * 0.25
+    seconds_deg = seconds * 0.0041667
+
+    # Sum up the degrees from all components
+    total_degrees = hours_deg + minutes_deg + seconds_deg
+
+    return total_degrees
+
+
+
+def convert_to_degrees(angle):
+    components = re.findall(r'[0-9]+(?:\.[0-9]+)?', angle)
+
+    degrees = int(components[0])
+    minutes = int(components[1])
+    seconds = float(components[2])
+
+    total_degrees = degrees + (minutes / 60) + (seconds / 3600)
+
+    return total_degrees
+
+
+def convert_to_3d_coordinates(right_ascension, inclination, distance):
+    # Convert angles from degrees to radians
+    ra_rad = math.radians(right_ascension)
+    inc_rad = math.radians(inclination)
+    if "," in distance:
+        distance = distance.replace(",", ".")
+    distance = float(distance)
+    # Calculate the 3D coordinates
+    x = distance * math.cos(ra_rad) * math.cos(inc_rad)
+    y = distance * math.sin(ra_rad) * math.cos(inc_rad)
+    z = distance * math.sin(inc_rad)
+
+    return x, y, z
+
+
+coordinates = []
+
+
+def process_file(csv_file="stars_and.csv", sep=','):
+    with open(csv_file, 'r', encoding="ISO-8859-1") as file:
+        # Create a CSV reader
+        reader = csv.reader(file, delimiter=sep)
+
+        # Read the CSV data row by row
+        for row in reader:
+            if (row[0].lower() == "name"):
+                continue
+            # Access the data elements in each row
+            # row[0] corresponds to the first column, row[1] to the second column, and so on
+            name = row[0]
+            if name == "":
+                continue
+            if len(row) < 3:
+                print(f"incorrect row {row}")
+            ra = convert_right_inclination_to_degrees(row[1])
+            inclination = convert_to_degrees(row[2])
+            ly = row[3]
+            if ly == "":
+                continue
+            x, y, z = convert_to_3d_coordinates(ra, inclination, ly)
+            #print(f"{x},{y},{z}")
+            coordinates.append((x, y, z))
+
+
+# Open the CSV file
+
+file_list = os.listdir(".")
+
+# Loop over the files that start with 'stars'
+for filename in file_list:
+    if filename.startswith("stars"):
+        print(filename)
+        process_file(filename)
+
+write_coordinates_to_csv("objects.csv", coordinates)
---
+++ objects.py
@@ -0,0 +1,42 @@
+import bpy
+import csv
+
+def is_number_tryexcept(s):
+    """ Returns True if string is a number. """
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+def create_sphere(x, y, z):
+    print(f"{x},{y},{z}")
+    bpy.ops.mesh.primitive_uv_sphere_add(radius=1, location=(float(x), float(y), float(z)))
+    sphere = bpy.context.object
+
+    # Assign the material to the sphere
+    sphere.data.materials.append(material)
+
+
+# Create the material (white)
+material = bpy.data.materials.new(name="White")
+material.use_nodes = True
+material.node_tree.nodes.clear()
+
+bsdf_node = material.node_tree.nodes.new(type="ShaderNodeBsdfPrincipled")
+bsdf_node.inputs['Base Color'].default_value = (1, 1, 1, 1)
+
+output_node = material.node_tree.nodes.new(type="ShaderNodeOutputMaterial")
+material.node_tree.links.new(bsdf_node.outputs['BSDF'], output_node.inputs['Surface'])
+
+
+with open("C:\\Users\\densh\\PycharmProjects\\starchart\\objects.csv", 'r') as file:
+    # Create a CSV reader
+    reader = csv.reader(file)
+    skip = True
+    for row in reader:
+        if skip:
+            skip = False
+            continue
+        x,y,z = row
+        create_sphere(x,y,z)
